cdc1bb26518e937c3597e20a7f431fcd0fbdc991
Sushmitaggit <susshmita101@yahoo.com>
2021-08-12 15:33:28 +0545

    memotype, reference and subject filter added.

--- kb-api/src/main/java/com/sb/solutions/api/creditmemo/repository/spec/CreditMemoTypeSpec.java
+++ kb-api/src/main/java/com/sb/solutions/api/creditmemo/repository/spec/CreditMemoTypeSpec.java
@@ -19,6 +19,7 @@ public class CreditMemoTypeSpec implements Specification<CreditMemo> {
     private static final String FILTER_BY_LOAN_ASSOCIATED = "loanAssociated";
     private static final String FILTER_BY_REFERENCE_NUMBER = "referenceNumber";
     private static final String FILTER_BY_DOC_STATUS = "documentStatus";
+    private static final String FITER_BY_SUBJECT="subject";
 
     private final String property;
     private final String value;
@@ -30,31 +31,34 @@ public class CreditMemoTypeSpec implements Specification<CreditMemo> {
     @Override
     public Predicate toPredicate(Root<CreditMemo> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
 
-            switch (property) {
-                case FILTER_BY_REFERENCE_NUMBER:
-                    return criteriaBuilder.like(criteriaBuilder.lower(root.get(FILTER_BY_REFERENCE_NUMBER)),
-                            "%" + value.toLowerCase() + "%");
-                case FILTER_BY_TYPE_ID:
-                    return criteriaBuilder.equal(root.join("type").get("id"), Long.parseLong(value));
-                case FILTER_BY_LOAN_ASSOCIATED:
-                    return criteriaBuilder.equal(root.get(property), false);
-                case FILTER_BY_CURRENT_STAGE_DATE:
-                    Gson gson = new Gson();
-                    Map dates = gson.fromJson(value, Map.class);
-                    try {
-                        return criteriaBuilder.between(root.get("createdAt"),
-                                new SimpleDateFormat("MM/dd/yyyy")
-                                        .parse(String.valueOf(dates.get("startDate"))),
-                                new SimpleDateFormat("MM/dd/yyyy")
-                                        .parse(String.valueOf(dates.get("endDate"))));
-                    } catch (ParseException e) {
-                        return null;
-                    }
-                case FILTER_BY_DOC_STATUS:
-                    return criteriaBuilder.equal(root.get(property), DocStatus.valueOf(value));
-                default:
+        switch (property) {
+            case FILTER_BY_REFERENCE_NUMBER:
+                return criteriaBuilder.like(criteriaBuilder.lower(root.get(FILTER_BY_REFERENCE_NUMBER)),
+                        "%" + value.toLowerCase() + "%");
+            case FILTER_BY_TYPE_ID:
+                return criteriaBuilder.equal(root.join("type").get("id"), Long.parseLong(value));
+            case FILTER_BY_LOAN_ASSOCIATED:
+                return criteriaBuilder.equal(root.get(property), false);
+            case FILTER_BY_CURRENT_STAGE_DATE:
+                Gson gson = new Gson();
+                Map dates = gson.fromJson(value, Map.class);
+                try {
+                    return criteriaBuilder.between(root.get("createdAt"),
+                            new SimpleDateFormat("MM/dd/yyyy")
+                                    .parse(String.valueOf(dates.get("startDate"))),
+                            new SimpleDateFormat("MM/dd/yyyy")
+                                    .parse(String.valueOf(dates.get("endDate"))));
+                } catch (ParseException e) {
                     return null;
-            }
+                }
+            case FILTER_BY_DOC_STATUS:
+                return criteriaBuilder.equal(root.get(property), DocStatus.valueOf(value));
+            case FITER_BY_SUBJECT:
+                return criteriaBuilder.like(criteriaBuilder.lower(root.get(FITER_BY_SUBJECT)),
+                        "%" + value.toLowerCase() + "%");
+            default:
+                return null;
         }
+    }
 
 }
